8bab347f507a386b3e3df164d6984703d23729db
jessenykzhu <jessenykzhu@tencent.com>
2020-07-29 16:33:17 +0800

    第一次作业

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a = 0, b = 0;
+    printf("Enter your integer:\n");
+    scanf("%d %d", &a, &b);
+    printf("%d\n%d\n", a, b);
+    return 0;
+}
+
+
